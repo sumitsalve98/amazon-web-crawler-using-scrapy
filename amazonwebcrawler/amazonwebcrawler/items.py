62500f3e1d16adad5d04bac046a419d6cdac445b
sumitsalve98 <sumitsalve4551@gmail.com>
2022-11-01 02:44:50 +0530

    product names scraped

--- amazonwebcrawler/amazonwebcrawler/items.py
+++ amazonwebcrawler/amazonwebcrawler/items.py
@@ -8,5 +8,5 @@ import scrapy
 
 class AmazonwebcrawlerItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    product_name = scrapy.Field()
+
--- amazonwebcrawler/amazonwebcrawler/spiders/amazoncrawler.py
+++ amazonwebcrawler/amazonwebcrawler/spiders/amazoncrawler.py
@@ -1,10 +1,13 @@
 import scrapy
+from ..items import AmazonwebcrawlerItem
 
 class amazon_crawler(scrapy.Spider):
-    name = "products"
+    name = "amazon_crawler"
     start_urls = [
         "https://www.amazon.in/s?k=data science book"
     ]
     def parse(self, response, **kwargs):
+        products = AmazonwebcrawlerItem()
         product_name = response.css(".a-color-base.a-text-normal::text").extract()
-        yield {"product_name": product_name}
+        products["product_name"] = product_name
+        yield products
